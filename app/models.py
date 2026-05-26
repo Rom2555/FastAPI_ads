d2fa7ef1c83bc528475ed84c380ba3fb4e3bc1c8
Roman E <rk2555@yandex.ru>
2026-05-26 16:31:06 +0300

    добавлена модель пользователя

--- app/models.py
+++ app/models.py
@@ -1,8 +1,26 @@
 from database import Base
-from sqlalchemy import Column, DateTime, Integer, String, Text, func
+from sqlalchemy import Column, DateTime, Integer, String, Text, func, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-# Модель база данных
+# Модель пользователя
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    # имя пользователя
+    username = Column(String(100), unique=True, nullable=False, index=True)
+    # Хеш пароля
+    password_hash = Column(String(255), nullable=False)
+    # Роль пользователя
+    role = Column(String(50), nullable=False, default="user")
+
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    # Связь с объявлениями
+    ads = relationship("Advertisement", back_populates='owner')
+
+
+# Модель объявления
 class Advertisement(Base):
     __tablename__ = "advertisements"
 
@@ -12,3 +30,7 @@ class Advertisement(Base):
     price = Column(Integer, nullable=False)
     author = Column(String(100), nullable=False)
     created_at = Column(DateTime, default=func.now(), nullable=False)
+    # ID владельца
+    owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    # Связь с пользователем
+    owner = relationship("User", back_populates="ads")
